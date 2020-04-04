30e398889bc983a9f234a9dd73daea4b6597b54c
L.Rain <bxy2997@sina.com>
2020-04-04 21:56:15 +0800

    use react hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,89 +1,30 @@
-import React, { useReducer, useEffect } from "react";
-import "../App.css";
-import Header from "./Header";
-import Movie from "./Movie";
-import Search from "./Search";
+import React, { useState, useEffect } from "react";
 
-const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b"; // you should replace this with yours
-
-const initialState = {
-  loading: true,
-  movies: [],
-  errorMessage: null
-};
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "SEARCH_MOVIES_REQUEST":
-      return {
-        ...state,
-        loadsing: true,
-        errprMessage: null
-      };
-    case "SEARCH_MOVIES_SUCCESS":
-      return {
-        ...state,
-        loading: false,
-        movies: action.payload
-      };
-    case "SEARCH_MOVIES_FAILURE":
-      return {
-        ...state,
-        loading: false,
-        errorMessage: action.error
-      };
-    default:
-      return state;
-  }
-};
-const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
+function App(props) {
+  const [count, setCount] = useState(0);
+  const testCount=useTest(count)
+  console.log(testCount)
   useEffect(() => {
-    fetch(MOVIE_API_URL)
-      .then(res => res.json())
-      .then(jsonRes => {
-        dispatch({
-          type: "SEARCH_MOVIES_SUCCESS",
-          payload: jsonRes.Search
-        });
-      });
-  }, []);
-
-  const search = searchValue => {
-    dispatch({ type: "SEARCH_MOVIES_REQUEST" });
-
-    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
-      .then(res => res.json())
-      .then(jsonRes => {
-        if (jsonRes.Response === "True") {
-          dispatch({ type: "SEARCH_MOVIES_SUCCESS", payload: jsonRes.Search });
-        } else {
-          dispatch({
-            type: "SEARCH_MOVIES_FAILURE",
-            error: jsonRes.Error
-          });
-        }
-      });
-  };
-  const {movies,errorMessage,loading}=state;
+    document.title = `You clicked ${count} time`;
+  });
   return (
-    <div className="App">
-      <Header text="HOOKED" />
-      <Search search={search} />
-      <p className="App-intro">Sharing a few of our favourite movies</p>
-      <div className="movies">
-        {loading && !errorMessage ? (
-          <span>loading...</span>
-        ) : errorMessage ? (
-          <div className="errorMessage">{errorMessage}</div>
-        ) : (
-          movies.map((movie, index) => (
-            <Movie key={`${index}-${movie.Title}`} movie={movie} />
-          ))
-        )}
-      </div>
+    <div>
+      <p>你点击了{count}次</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        点击
+      </button>
     </div>
   );
-};
-
+}
+function useTest(count){
+  const [doubleCount,setDoubleCount]=useState(count)
+  useEffect(()=>{
+    console.log(12312)
+  })
+  return doubleCount
+}
 export default App;
